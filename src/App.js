361e61533d1f8aab0889ce81a648e5aeda0c0732
Tomilov Sergey <tomexpert@yandex.ru>
2022-03-23 19:08:39 +0800

    v1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
 import Menu from './Menu/Menu';
 import HomePage from './Menu/HomePage';
@@ -10,14 +10,14 @@ import ForzaPage from './Menu/ForzaPage';
 export default function App() {
   return (
     <BrowserRouter>
-      <div>
-        <Menu />
-        <div className="page">
-          <Route path="/" exact component={HomePage} />
-          <Route path="/drift" component={DriftPage} />
-          <Route path="/timeattack" component={TimeAttackPage} />
-          <Route path="/forza" component={ForzaPage} />
-        </div>
+      <Menu />
+      <div className="page">
+        <Routes>
+          <Route path="/" exact element={<HomePage />} />
+          <Route path="/drift" element={<DriftPage />} />
+          <Route path="/timeattack" element={<TimeAttackPage />} />
+          <Route path="/forza" element={<ForzaPage />} />
+        </Routes>
       </div>
     </BrowserRouter>
   );
--- src/Menu/Menu.jsx
+++ src/Menu/Menu.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from "react-router-dom";
 
 function Menu() {
   return (
-    <div>Menu</div>
+    <nav className="menu">
+      <NavLink className={({ isAstive1 }) => isAstive1 ? 'menu__item-active menu__item' : "menu__item"} to="/">Главная</NavLink>
+      <NavLink className={({ isAstive }) => isAstive ? 'menu__item-active menu__item' : "menu__item"} to="/drift">Дрифт-такси</NavLink>
+      <NavLink className={({ isAstive }) => isAstive ? 'menu__item-active menu__item' : "menu__item"} to="/timeattack">Time Attack</NavLink>
+      <NavLink className={({ isAstive }) => isAstive ? 'menu__item-active menu__item' : "menu__item"} to="/forza">Forza Karting</NavLink>
+    </nav>
   )
 }
 
