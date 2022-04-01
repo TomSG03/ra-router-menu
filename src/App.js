f56c766e5a54a4a439f6128e57913452853d8bdd
Tomilov Sergey <tomexpert@yandex.ru>
2022-04-02 00:36:04 +0800

    fix path

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import './App.css';
 import Menu from './Menu/Menu';
 import HomePage from './Menu/HomePage';
@@ -9,16 +9,18 @@ import ForzaPage from './Menu/ForzaPage';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Menu />
-      <div className="page">
-        <Routes>
-          <Route path="/" exact element={<HomePage />} />
-          <Route path="/drift" element={<DriftPage />} />
-          <Route path="/timeattack" element={<TimeAttackPage />} />
-          <Route path="/forza" element={<ForzaPage />} />
-        </Routes>
+    <HashRouter>
+      <div>
+        <Menu />
+        <div className="page">
+          <Routes>
+            <Route path="/drift" element={<DriftPage />} />
+            <Route path="/timeattack" element={<TimeAttackPage />} />
+            <Route path="/forza" element={<ForzaPage />} />
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </div>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
--- src/Menu/Menu.jsx
+++ src/Menu/Menu.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { NavLink } from "react-router-dom";
 
 function Menu() {
+
   return (
     <nav className="menu">
-      <NavLink className={({ isAstive1 }) => isAstive1 ? 'menu__item-active menu__item' : "menu__item"} to="/">Главная</NavLink>
-      <NavLink className={({ isAstive }) => isAstive ? 'menu__item-active menu__item' : "menu__item"} to="/drift">Дрифт-такси</NavLink>
-      <NavLink className={({ isAstive }) => isAstive ? 'menu__item-active menu__item' : "menu__item"} to="/timeattack">Time Attack</NavLink>
-      <NavLink className={({ isAstive }) => isAstive ? 'menu__item-active menu__item' : "menu__item"} to="/forza">Forza Karting</NavLink>
+      <NavLink className={({ isActive }) => isActive ? "menu__item-active menu__item" : "menu__item"} to="/">Главная</NavLink>
+      <NavLink className={({ isActive }) => isActive ? "menu__item-active menu__item" : "menu__item"} to="/drift">Дрифт-такси</NavLink>
+      <NavLink className={({ isActive }) => isActive ? "menu__item-active menu__item" : "menu__item"} to="/timeattack">Time Attack</NavLink>
+      <NavLink className={({ isActive }) => isActive ? "menu__item-active menu__item" : "menu__item"} to="/forza">Forza Karting</NavLink>
     </nav>
   )
 }
